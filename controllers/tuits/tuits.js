ad279d261949d63d84d2eb61c20f4cec2047f1af
Heyang Chen <yuelin@iMac.local>
2022-11-19 15:53:03 -0800

    update

--- controllers/tuits/tuits.js
+++ controllers/tuits/tuits.js
@@ -3,7 +3,7 @@ export default [
     "_id": 123, "topic": "Space", "username": "SpaceX",
     "handle": "@spacex", "time": "2h", "image": "https://user-images.githubusercontent.com/113388766/202819158-8d68b235-34d2-41ac-a043-ddfbe06f3e94.jpg",
     "title": "SpaceX's Mission",
-    "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
+    "content": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars",
     "liked": true, "likes": 2345, "dislikes": 5,
     "replies": 123, "retuits": 432
   },
@@ -11,7 +11,7 @@ export default [
     "_id": 234, "topic": "Traffic", "username": "The Boring Company",
     "handle": "@boringcompany", "time": "2h", "image": "https://user-images.githubusercontent.com/113388766/202819158-8d68b235-34d2-41ac-a043-ddfbe06f3e94.jpg",
     "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
-    "tuit": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
+    "content": "The Boring Company (TBC) is an American infrastructure and tunnel construction services company founded by Elon Musk. Its ongoing and proposed projects are designed for intra-city ('loop') transit systems.",
     "liked": true, "likes": 2345, "dislikes": 0,
     "replies": 123, "retuits": 432
   },
